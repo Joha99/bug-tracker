3ce39acddc0f6cfdeeda1eecb82cf9c9ce267183
Joha Kim <johakim9936@gmail.com>
2021-03-22 20:19:33 -0400

    Use redux toolkit to create store

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
-// dispatch action to store
+// dispatch actions to store
 store.dispatch(actions.bugAdded("Bug 1"));
 store.dispatch(actions.bugAdded("Bug 2"));
 store.dispatch(actions.bugAdded("Bug 3"));

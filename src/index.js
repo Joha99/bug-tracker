51073163ed9baf557620ef4c1be30b3b2fdeb1e1
Joha Kim <johakim9936@gmail.com>
2021-03-24 11:48:08 -0400

    Create another reducer for projects

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import * as projectActions from "./store/projects"; 
 
 const store = configureStore();
 console.log(store);
 
 // subscribe to store to detect changes
 const unsubscribe = store.subscribe(() => {
-  console.log("Store changed", store.getState());
+  console.log("Store changed!", store.getState());
 });
 
 // dispatch actions to store
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+// store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
+store.dispatch(projectActions.projectAdded({description: "Project 1"}));
+
 
 unsubscribe();
---
+++ src/store/projects.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// create initial state
+const initialState = [];
+let lastId = 0;
+
+// create actions & reducers
+const projectSlice = createSlice({
+  name: "projects",
+  initialState: initialState,
+  reducers: {
+    projectAdded: (state, action) => {
+      state.push({ id: ++lastId, description: action.payload.description });
+    },
+  },
+});
+
+// export as default the reducer
+export default projectSlice.reducer;
+
+// export actions
+export const { projectAdded } = projectSlice.actions;

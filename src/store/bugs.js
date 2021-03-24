5ad97da4197ad5c80b02060cc13b4c110f56dce6
Joha Kim <johakim9936@gmail.com>
2021-03-24 11:12:35 -0400

    Use redux's createSlice to create actions and reducers at the same time

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,43 +1,25 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
-// ACTION CREATORS
-//////////////////////////////////////////
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-// REDUCER
-//////////////////////////////////////////
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: lastId++,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-
-    case bugResolved.type:
-      return state.map((bug) => {
-        if (bug.id === action.payload.id) {
-          // create a copy of the bug instance rather than changing the bug itself
-          return {
-            ...bug,
-            resolved: true,
-          };
-        }
-        return bug;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (state, action) => {
+      state.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
       });
+    },
+    bugResolved: (state, action) => {
+      const idx = state.findIndex((bug) => bug.id === action.payload.id);
+      state[idx].resolved = true;
+    },
+  },
+});
+console.log(slice);
 
-    default:
-      return state;
-  }
-}
+export default slice.reducer;
+export const { bugAdded, bugResolved } = slice.actions;

8d31114404342f9278bc84926ff9fc54a5a491cf
Joha Kim <johakim9936@gmail.com>
2021-03-25 16:54:10 -0400

    parameterize custom middleware

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,5 +1,6 @@
-const logger = (state) => (next) => (action) => {
-  console.log(state, next, action);
+const logger = (param) => (state) => (next) => (action) => {
+  console.log("logging to: ", param);
+  next(action);
 };
 
 export default logger;
